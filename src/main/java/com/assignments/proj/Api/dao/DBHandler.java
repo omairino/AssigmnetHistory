b6700d3911fbe56d97068ed318c7380af58c6f3b
tmeraslan <tmeraslan1@gmail.com>
2019-09-03 09:52:49 +0300

    add search

--- src/main/java/com/assignments/proj/Api/dao/DBHandler.java
+++ src/main/java/com/assignments/proj/Api/dao/DBHandler.java
@@ -16,7 +16,7 @@ import java.util.Properties;
 
     private DBHandler() {
 
-        try (FileInputStream fis = new FileInputStream("C:\\Users\\omair\\Desktop\\project amazon\\omair\\db.config")) {
+        try (FileInputStream fis = new FileInputStream("C:\\Users\\edwan\\IdeaProjects\\AssigmnetHistory\\db.config")) {
             properties = new Properties();
 
             properties.load(fis);
