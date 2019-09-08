478add009bb9ea23e023e9db2cf6a9be1ea91ce3
Administrator <omair.issa6@gmail.com>
2019-09-08 14:31:43 +0300

    fix Project

--- src/main/java/com/assignments/proj/Api/model/Project.java
+++ src/main/java/com/assignments/proj/Api/model/Project.java
@@ -1,55 +1,73 @@
 package com.assignments.proj.Api.model;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 public class Project {
 
     private int id;
+    private long ManagerId;
     private String projectName;
     private String description;
+    private Date startDate;
+    List<Skill> skills;
 
-    public String getDescription() {
-        return description;
+    public Project(int id, long managerId, String projectName, String description, Date startDate, List<Skill> skills) {
+        this.id = id;
+        ManagerId = managerId;
+        this.projectName = projectName;
+        this.description = description;
+        this.startDate = startDate;
+        this.skills = skills;
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getManagerId() {
+        return ManagerId;
+    }
+
+    public void setManagerId(long managerId) {
+        ManagerId = managerId;
+    }
+
     public String getProjectName() {
         return projectName;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setProjectid(int projectId) {
-        this.id = projectId;
     }
 
     public void setProjectName(String projectName) {
         this.projectName = projectName;
     }
 
-    public Project(int projectId, String projectName , String description) {
-        this.id = projectId ;
-        this.projectName = projectName;
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
         this.description = description;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Project project = (Project) o;
-        return id == project.id &&
-                Objects.equals(projectName, project.projectName) &&
-                Objects.equals(description, project.description);
+    public Date getStartDate() {
+        return startDate;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, projectName, description);
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
     }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+
 }
