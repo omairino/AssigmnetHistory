f90deb0ba090da2cab958158fff1f874e56756a8
Administrator <omair.issa6@gmail.com>
2019-09-15 10:31:31 +0300

    fix projecController

--- src/main/java/com/assignments/proj/Api/controller/ProjectController.java
+++ src/main/java/com/assignments/proj/Api/controller/ProjectController.java
@@ -3,9 +3,9 @@ package com.assignments.proj.Api.controller;
 import java.sql.SQLException;
 import java.util.List;
 
-import com.assignments.proj.Api.dao.AddProjectDAO;
+
 import com.assignments.proj.Api.dao.IProjectDAO;
-import com.assignments.proj.Api.model.AddProject;
+import com.assignments.proj.Api.dao.ProjectsDAO;
 import com.assignments.proj.Api.model.Assignment;
 import com.assignments.proj.Api.model.Project;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +21,7 @@ public class ProjectController {
     @Autowired
     private IProjectDAO projectsDAO;
     @Autowired
-    private AddProjectDAO addProjectDAO;
+    private ProjectsDAO addProjectDAO;
 
     @GetMapping("")
     public ResponseEntity<List<Project>> getProjects() throws SQLException {
@@ -33,8 +33,8 @@ public class ProjectController {
         return new ResponseEntity<>(projectsDAO.getManagerProjects(managerID), HttpStatus.OK);
     }
 
-    @PostMapping("/addproject")
-    public ResponseEntity<AddProject> addProject(@RequestBody AddProject project) throws SQLException {
+    @PostMapping("")
+    public ResponseEntity<Project> addProject(@RequestBody Project project) throws SQLException {
         return new ResponseEntity<>(addProjectDAO.add(project), HttpStatus.OK);
     }
 }
